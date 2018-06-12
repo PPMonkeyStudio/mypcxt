0f49e1e4a4473479fdc545410b9340fba611585a
ZB <www.1037976812@qq.com>
2018-06-13 05:29:52 +0800

    1

--- src/com/pphgzs/dao/impl/QuestionDaoImpl.java
+++ src/com/pphgzs/dao/impl/QuestionDaoImpl.java
@@ -249,12 +249,8 @@ public class QuestionDaoImpl implements QuestionDao {
 	public List<mypcxt_question> list_Question_byDefinitionID(String definitionID) {
 
 		Session session = getSession();
-<<<<<<< HEAD
-		String hql = "from mypcxt_question where question_service_definition > '" + definitionID
+		String hql = "from mypcxt_question where question_service_definition = '" + definitionID
 				+ "' order by question_sort desc";
-=======
-		String hql = "from mypcxt_question where question_service_definition = '" + definitionID + "' order by question_sort desc";
->>>>>>> origin/LWK
 		Query query = session.createQuery(hql);
 		List<mypcxt_question> questionList = query.list();
 		return questionList;
@@ -292,7 +288,8 @@ public class QuestionDaoImpl implements QuestionDao {
 	@Override
 	public List<mypcxt_question> list_QuestionMax_byDefinitionID(String question_service_definition) {
 		Session session = getSession();
-		String hql = "from mypcxt_question where question_service_definition > '" + question_service_definition + "' order by question_sort desc";
+		String hql = "from mypcxt_question where question_service_definition > '" + question_service_definition
+				+ "' order by question_sort desc";
 		Query query = session.createQuery(hql);
 		query.setFirstResult(0);
 		query.setMaxResults(1);
